383a2a47ce2e5a93bb8db2d8bda04fb88e5e8654
Arqam Usman Ali <aa358@njit.edu>
2021-12-16 00:09:29 -0500

    Update calculator.py

--- calc/calculator.py
+++ calc/calculator.py
@@ -1,20 +1,13 @@
 """ This is the increment function"""
 from calc.history.calculations import Calculations
-
-
-# the calculator class just contains the methods to calculate
 class Calculator:
     """ This is the Calculator class"""
-
-    # the calculator class just calls methods on Calculations class
     @staticmethod
     def get_last_result_value():
         """ This is the gets the result of the calculation"""
-        # I made this method so that I don't have more than one action per function
         return Calculations.get_last_calculation_result_value()
 
     @staticmethod
-    # tuple allows me to pass in as many values as I want
     def addition(tuple_values: tuple):
         """ adds list of numbers"""
         Calculations.add_addition(tuple_values)
